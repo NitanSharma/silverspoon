05a9d5ba73f52543b703b888a00c9ff0ccc89b70
NitanSharma <sharmanitan938@gmail.com>
2025-08-16 16:09:08 +0530

    Footer Component Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 
 export default function page() {
   return (
     <main className="bg-[#FFF9E9] w-full min-h-screen ">
         <HeroSection />
+        <Footer/>
     </main>
   );
 }
