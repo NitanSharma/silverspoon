2065d98617cd90006068a566ff9453df36427920
kunal <monupanwar5655@gmail.com>
2025-08-16 12:23:45 +0530

    howitwork

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/HeroSection";
+import HowItWork from "@/components/HowItWork";
 
 export default function page() {
   return (
     <main className="bg-[#FFF9E9] w-full min-h-screen ">
         <HeroSection />
+        <HowItWork/>
     </main>
   );
 }
